c9c7d5c0c34d689e27e0ab2bd5d8a3b0f036dd2e
Ttabizt1337 <krzy2019@wp.pl>
2024-05-21 23:46:51 +0200

    Updated scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const scripts = [
-        { name: "Back", url: "scripts/Back.lua" },
-        { name: "command", url: "scripts/command.lua" },
-        { name: "configure", url: "scripts/configure.lua" },
-        { name: "Logo", url: "scripts/Logo.nfp" },
-        { name: "LogoCC", url: "scripts/LogoCC.nfp" },
-        { name: "MPrograms", url: "scripts/MPrograms.lua" },
-        { name: "programs", url: "scripts/programs.lua" },
-        { name: "starter", url: "scripts/starter.lua" },
-        { name: "startup", url: "scripts/startup.lua" },
-        { name: "startupFile", url: "scripts/startupFile.lua" },
-        { name: "system", url: "scripts/system.lua" },
-        { name: "Uninstall", url: "scripts/Uninstall.lua" },
-        { name: "updateOS", url: "scripts/updateOS.lua" },
-        { name: "UpdatePopup", url: "scripts/UpdatePopup.lua" }
+        { name: "Offline Installer", url: "scripts/OfflineInstaller.lua" },
     ];
 
     const scriptList = document.getElementById("script-list");
